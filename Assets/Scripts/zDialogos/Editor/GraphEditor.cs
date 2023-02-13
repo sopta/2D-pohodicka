f80723e1b78111230d25e2dd9b3f35ce89e4b5c1
sopta <sotolar.zdenek@gmail.com>
2023-02-13 10:32:04 +0100

    editor btn

--- Assets/Scripts/zDialogos/Editor/GraphEditor.cs
+++ Assets/Scripts/zDialogos/Editor/GraphEditor.cs
@@ -2,6 +2,7 @@ using UnityEditor;
 using UnityEditor.UIElements;
 using UnityEngine;
 using UnityEngine.UIElements;
+using XNodeEditor;
 
 [CustomEditor(typeof(DialogueNodeGraph))]
 public class GraphEditor : Editor
@@ -9,6 +10,16 @@ public class GraphEditor : Editor
     public override VisualElement CreateInspectorGUI()
     {
         var root = new VisualElement();
+        var showGraphBtn = new Button();
+        showGraphBtn.text = "Edit graph";
+        showGraphBtn.style.height = 40;
+        showGraphBtn.clickable.clicked += () =>
+        {
+            NodeEditorWindow.Open(serializedObject.targetObject as XNode.NodeGraph);
+        };
+        
+        root.Add(showGraphBtn);
+        
         InspectorElement.FillDefaultInspector(root, serializedObject, this);
 
         var btn = new Button();
